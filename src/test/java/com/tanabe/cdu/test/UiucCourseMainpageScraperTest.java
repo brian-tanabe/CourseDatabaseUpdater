015a64dd628a1861ca446c5e8dacfb0792e2551b
Brian Tanabe <btanabe2@gmail.com>
2014-08-23 19:35:15 -0500

    Initial commit

---
+++ src/test/java/com/tanabe/cdu/test/UiucCourseMainpageScraperTest.java
@@ -0,0 +1,15 @@
+package com.tanabe.cdu.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Brian on 8/23/14.
+ */
+public class UiucCourseMainpageScraperTest {
+
+
+    @Test
+    public void testCreate(){
+        UiucCourseMainpageScraper scraper = new UiucCourseMainpageScraper();
+    }
+}
